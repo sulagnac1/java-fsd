9ec130f7beba97f22786cde0dd505f40b5831593
sulagnac1 <132747156+sulagnac1@users.noreply.github.com>
2023-05-08 12:09:18 +0530

    Add files via upload

---
+++ Collection.java
@@ -0,0 +1,18 @@
+package demo;
+import java.util.*;
+
+public class Collection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = new int[] {10, 20, 30, 40};
+		Vector<Integer> v = new Vector();
+		v.add(1);
+		v.add(2);
+		System.out.println(arr[0]);
+		System.out.println(v.elementAt(0));
+		
+
+	}
+
+}
